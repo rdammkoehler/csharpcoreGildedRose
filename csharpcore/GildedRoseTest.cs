36e5208b147ed1ab293543224feb5785225ae44c
Rich Dammkoehler <rpd@noradltd.com>
2018-10-20 15:05:20 -0500

    characterize: quality is never reduced below zero

--- csharpcore/GildedRoseTest.cs
+++ csharpcore/GildedRoseTest.cs
@@ -7,20 +7,20 @@ namespace csharpcore
     {
         private GildedRose App { get; set; }
         private Item[] Items { get; set; }
-        
+
         private void Initialize(params Item[] Items)
         {
             this.Items = Items;
             App = new GildedRose(Items);
         }
-        
+
         [Fact]
         public void ctQualityCanBeNegativeIfSellInGreaterThanZero()
         {
             Initialize(new Item {Name = "foo", SellIn = 10, Quality = -1});
 
             App.UpdateQuality();
-            
+
             Assert.Equal(-1, Items[0].Quality);
         }
 
@@ -30,8 +30,18 @@ namespace csharpcore
             Initialize(new Item {Name = "foo", SellIn = 10, Quality = 5});
 
             App.UpdateQuality();
-            
-            Assert.Equal(4, Items[0].Quality);            
+
+            Assert.Equal(4, Items[0].Quality);
+        }
+
+        [Fact]
+        public void ctQualityDoesNotDecreaseBelowZero()
+        {
+            Initialize(new Item {Name = "foo", SellIn = 10, Quality = 0});
+
+            App.UpdateQuality();
+
+            Assert.Equal(0, Items[0].Quality);
         }
     }
 }
