e66707c059d8a0b41d11a0282a61a5dba9f771bc
Rich Dammkoehler <rpd@noradltd.com>
2018-10-22 01:09:46 -0400

    refactor: reverse most of the negative logic to create readability

--- csharpcore/GildedRose.cs
+++ csharpcore/GildedRose.cs
@@ -5,6 +5,7 @@ namespace csharpcore
     public class GildedRose
     {
         IList<Item> Items;
+
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
@@ -14,17 +15,7 @@ namespace csharpcore
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
+                if (Items[i].Name == "Aged Brie" || Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (Items[i].Quality < 50)
                     {
@@ -50,6 +41,17 @@ namespace csharpcore
                         }
                     }
                 }
+                else
+                {
+                    if (Items[i].Quality > 0)
+                    {
+                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            Items[i].Quality = Items[i].Quality - 1;
+                        }
+                    }
+                }
+
 
                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                 {
@@ -58,9 +60,20 @@ namespace csharpcore
 
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (Items[i].Name == "Aged Brie")
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].Quality < 50)
+                        {
+                            Items[i].Quality = Items[i].Quality + 1;
+                        }
+                    }
+                    else
+                    {
+                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                        }
+                        else
                         {
                             if (Items[i].Quality > 0)
                             {
@@ -69,17 +82,6 @@ namespace csharpcore
                                     Items[i].Quality = Items[i].Quality - 1;
                                 }
                             }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
                         }
                     }
                 }
